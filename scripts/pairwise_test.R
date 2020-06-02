6c5c2fccde3b3f0d17cba9db6040c9dc226b6c62
Tatum Mortimer <tatumdmortimer@gmail.com>
2020-06-02 18:34:05 -0400

    log transformed MICs

--- scripts/pairwise_test.R
+++ scripts/pairwise_test.R
@@ -1,4 +1,4 @@
 library(tidyverse)
 
 paired_samples <- read_tsv("paired_by_distance.txt")
-wilcox.test(paired_samples$mtrD_azi_mic, paired_samples$wt_azi_mic, paired = TRUE)
+wilcox.test(log2(paired_samples$mtrD_azi_mic), log2(paired_samples$wt_azi_mic), paired = TRUE)
