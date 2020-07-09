0be3fb623d905ceca076b5cd6c59ddbea3c6e271
Tatum Mortimer <tatumdmortimer@gmail.com>
2020-07-09 12:08:40 -0400

    updated closest strain script to be useful for any antibiotic

--- scripts/get_closest_strain.py
+++ scripts/get_closest_strain.py
@@ -9,6 +9,8 @@ def get_args():
     parser.add_argument("matrix", help="Distances of all mtrD mutants to other strains")
     parser.add_argument("strains", help="TSV with each strain and its mtrD mutation")
     parser.add_argument("metadata", help="metadata file with MICs")
+    parser.add_argument("antibiotic", help="anbiotic name for output file")
+    parser.add_argument("mic_column", help="column number (0 index) for MIC in metadata file", type = int)
     return parser.parse_args()
 
 def read_distances(matrix_file):
@@ -32,18 +34,18 @@ def get_mtrD_strains(mtrD_file):
             mtrD_strains[line[0]] = line[1]
     return mtrD_strains
 
-def get_azi(metadata_file):
+def get_azi(metadata_file, mic_column):
     """Reads metadata file (specifically strain-table-filtered.csv)"""
-    azi_mic = {}
+    mic = {}
     with open(metadata_file, "r") as infile:
         for line in infile:
             line = line.strip().split(',')
-            azi_mic[line[1]] = line[9]
-    return azi_mic
+            mic[line[1]] = line[mic_column]
+    return mic
 
-def find_closest(distances, mtrD_strains, azi_mic):
+def find_closest(distances, mtrD_strains, mic, antibiotic):
     """Finds closest strain that does not also have mtrD mutation and has azithromycin MIC"""
-    with open("distances/paired_by_distance.txt", "w") as outfile:
+    with open(f"distances/paired_by_distance_{antibiotic}.txt", "w") as outfile:
         outfile.write("mtrD_strain\tmtrD_mutation\tmtrD_azi_mic\twt_strain\twt_azi_mic\tSNP_distance\n")
         for strain in distances:
             distance_dict = distances[strain]
@@ -63,8 +65,8 @@ def main():
     args = get_args()
     distances = read_distances(args.matrix)
     mtrD_strains = get_mtrD_strains(args.strains)
-    azi_mic = get_azi(args.metadata)
-    find_closest(distances, mtrD_strains, azi_mic)
+    mic = get_mic(args.metadata, args.mic_column)
+    find_closest(distances, mtrD_strains, mic, args.antibiotic)
 
 if __name__ == "__main__":
     main()
